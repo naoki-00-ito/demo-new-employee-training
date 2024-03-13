b48eb25e7c4794c2a361f205828c757129b0a1df
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-03-14 07:54:39 +0900

    [Update] 画像パス修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
+const BASE_PATH = '';
+
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -17,7 +19,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src='/vercel.svg'
+              src={`${BASE_PATH}/vercel.svg`}
               alt='Vercel Logo'
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +33,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src='/next.svg'
+          src={`${BASE_PATH}/vercel.svg`}
           alt='Next.js Logo'
           width={180}
           height={37}

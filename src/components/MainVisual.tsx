8a8648f9c5d6794f48f5d785ed6a8f5884623107
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-03-16 22:24:55 +0900

    [Add] fadein

--- src/components/MainVisual.tsx
+++ src/components/MainVisual.tsx
@@ -1,9 +1,17 @@
+'use client';
+
+import { useRef } from 'react';
 import { Flex, Box, Heading, Text } from '@chakra-ui/react';
+import useFadeIn from '@/hooks/useFadeIn';
 
 const MainVisual = () => {
+  const ref = useRef(null);
+
+  useFadeIn({ ref: ref });
+
   return (
     <Flex justifyContent={'center'} alignItems={'center'} height={'100vh'}>
-      <Box display={'inline-block'}>
+      <Box display={'inline-block'} ref={ref}>
         <Heading as={'h1'} textAlign={'center'}>
           DEMO APP
         </Heading>

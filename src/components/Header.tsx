265a958af96b0b6a33cad84c5df304d922333b04
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2024-03-16 20:41:03 +0900

    [Update] link 変数切り出し

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,6 +13,7 @@ import {
   ListItem,
   Link,
 } from '@chakra-ui/react';
+import links from '@/data/links';
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -35,21 +36,15 @@ const Header = () => {
           <DrawerHeader borderBottomWidth='1px'>Menu</DrawerHeader>
           <DrawerBody>
             <UnorderedList>
-              <ListItem>
-                <Link color='teal.500' href='/html'>
-                  HTML Only
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link color='teal.500' href='/html-css'>
-                  HTML + CSS
-                </Link>
-              </ListItem>
-              <ListItem>
-                <Link color='teal.500' href='/html-css-js'>
-                  HTML + CSS + JavaScript
-                </Link>
-              </ListItem>
+              {links.map((link, i) => {
+                return (
+                  <ListItem key={i}>
+                    <Link color='teal.500' href={link.href}>
+                      {link.name}
+                    </Link>
+                  </ListItem>
+                );
+              })}
             </UnorderedList>
           </DrawerBody>
         </DrawerContent>
---
+++ src/data/links.ts
@@ -0,0 +1,16 @@
+const links = [
+  {
+    name: 'HTML Only',
+    href: '/html',
+  },
+  {
+    name: 'HTML + CSS',
+    href: '/html-css',
+  },
+  {
+    name: 'HTML + CSS + JavaScript',
+    href: '/html-css-js',
+  },
+];
+
+export default links;
